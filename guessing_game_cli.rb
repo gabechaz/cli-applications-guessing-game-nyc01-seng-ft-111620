6f8bb24b05bc4830a14f7042563db09123a63a37
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-11-12 23:18:42 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,5 +1,9 @@
 # Code your solution here!
 def run_guessing_game
   el_rando = rand(6) + 1
-  
+  puts "Can you guess a number between one and six?"
+  user_input = gets.chomp
+  if user_input == el_rando
+    
+end
 end
