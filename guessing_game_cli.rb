0fc5b6101cd088f55e41641ec191e7c8a41a99ca
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-11-12 23:20:01 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,6 +5,8 @@ def run_guessing_game
   user_input = gets.chomp
   if user_input == el_rando
     "You guessed the correct number!"
+    elsif user_input == exit
+    puts "Goodbye!"
   else
     puts "Sorry. The computer guessed #{el_rando}."
 end
